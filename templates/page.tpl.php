e3c4f99ca94bfa6e80affb5f489db031008fdf4b
ada_messer <ladydowaakhin@gmail.com>
2016-06-24 23:37:17 +0300

    after code formatting

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,15 +1,15 @@
 <div id="header">
-    <?php print render($page['header']); ?>
+  <?php print render($page['header']); ?>
 </div>
 
 <div id="wrapper">
-    <div id="content">
-        <?php print render($page['content']); ?>
-        <?php t('<h1>hello,world!!!</h1>') ?>
-    </div>
+  <div id="content">
+    <?php print render($page['content']); ?>
+    <?php t('<h1>hello,world!!!</h1>') ?>
+  </div>
 
 </div>
 
 <div id="footer">
-        <?php print render($page['footer']); ?>
+  <?php print render($page['footer']); ?>
 </div>
